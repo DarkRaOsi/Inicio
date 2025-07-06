554fbd32dc0be57e13008a50c972f8f4a6df22f1
DESKTOP-1O2ID01\darru <darrufrod@gmail.com>
2025-07-06 20:52:18 +0200

    Logica de menú de pausa y button de exit

--- Proyect_Ing_Software/Assets/Scripts/Menu.cs
+++ Proyect_Ing_Software/Assets/Scripts/Menu.cs
@@ -5,13 +5,19 @@ using UnityEngine.UI; // Importa el namespace para la gestión de escenas
 public class Menu : MonoBehaviour
 {
     public Button PlayButton;
+    public Button ExitButton;
     // Función pública para cargar una escena por su nombre o índice
+    public void Start()
+    {
+        PlayButton.onClick.AddListener(StartGame);
+        ExitButton.onClick.AddListener(QuitGame);
+    }
     public void StartGame()
     {
         SceneManager.LoadScene("Game");
     }
-    public void Start()
+    public void QuitGame()
     {
-        PlayButton.onClick.AddListener(StartGame);
+        Application.Quit();
     }
 }
